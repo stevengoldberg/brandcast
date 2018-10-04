0060b1a76aba1b9b59def8ccd9a6b3625b72105b
Steve Goldberg <steve.schnee@gmail.com>
2018-10-04 14:46:21 -0700

    Rm debugger

--- src/NodeView.js
+++ src/NodeView.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react'
 
 class NodeView extends Component {
   render() {
-    const { node, addChild } = this.props
+    const { node, addChild, removeNode } = this.props
     return (
       <div className="row">
         {node.props.name}
         <div className="add" onClick={() => addChild(node.id)}>+</div>
+        <div className="remove" onClick={() => {
+          removeNode(node.id)
+        }}>-</div>
       </div>
     )
   }
